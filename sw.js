3f171b495af13c5fa757b44d1d1d1aa20306eb4d
lennoxlewis2 <lewiscurtis2@hotmail.co.uk>
2026-06-23 19:10:20 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'sr-tracker-v3';
-const FILES = ['/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const FILES = ['/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
